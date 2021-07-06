e7eb915787c015e416cbade7def101a277640e0a
Jürgen Brüder <juergen.brueder@gmail.com>
2021-07-06 16:09:34 +0200

    feat: add ENV-GITLAB-DOMAIN

--- replace-variables.sh
+++ replace-variables.sh
@@ -5,3 +5,6 @@ find deployment* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
 
 # Replace <ENV-ANIMAL>
 find deployment* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
+
+# Replace <ENV-GITLAB-DOMAIN>
+find deployment* -type f -exec sed -i -e "s/<ENV-GITLAB-DOMAIN>/$GITLAB_DOMAIN/g" {} \;

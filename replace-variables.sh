a5848a13f512c778f1f327c98df37b5027e78694
Jürgen Brüder <juergen.brueder@gmail.com>
2023-03-29 15:48:37 +0200

    refactor: adapted to k3s and new flux version

--- replace-variables.sh
+++ replace-variables.sh
@@ -5,6 +5,3 @@ find deployment* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
 
 # Replace <ENV-ANIMAL>
 find deployment* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
-
-# Replace <ENV-GITLAB-DOMAIN>
-find deployment* -type f -exec sed -i -e "s/<ENV-GITLAB-DOMAIN>/$GITLAB_DOMAIN/g" {} \;

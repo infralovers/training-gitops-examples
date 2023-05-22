720a5cdb1bf796aa7d0368d2e8a630c3a883d7bc
Martin Buchleitner <mbuchleitner@infralovers.com>
2023-05-22 14:14:00 +0200

    fix: update script and perm on the script
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- replace-variables.sh
+++ replace-variables.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Replace <ENV-USER-ID>
-find examples* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
+find deployment* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
 
 # Replace <ENV-ANIMAL>
-find examples* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
+find deployment* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
 
 # Replace <ENV-IP>
-find examples* -type f -exec sed -i -e "s/<ENV-IP>/$HOST_IP/g" {} \;
+find deployment* -type f -exec sed -i -e "s/<ENV-IP>/$HOST_IP/g" {} \;
 
 # Replace <ENV-NAME>
-find examples* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
+find deployment* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
 
 # Replace <ENV-DOMAIN>
-find examples* -type f -exec sed -i -e "s/<ENV-DOMAIN>/$DOMAIN/g" {} \;
+find deployment* -type f -exec sed -i -e "s/<ENV-DOMAIN>/$DOMAIN/g" {} \;

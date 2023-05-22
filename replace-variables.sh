9bf596ce90c3ee5c1f4fdc6591b690f430dc8861
Martin Buchleitner <mbuchleitner@infralovers.com>
2023-05-22 14:00:16 +0200

    fix: dns name update
    Signed-off-by: Martin Buchleitner <mbuchleitner@infralovers.com>

--- replace-variables.sh
+++ replace-variables.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 # Replace <ENV-USER-ID>
-find deployment* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
+find examples* -type f -exec sed -i -e "s/<ENV-USER-ID>/$USER_ID/g" {} \;
 
 # Replace <ENV-ANIMAL>
-find deployment* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
+find examples* -type f -exec sed -i -e "s/<ENV-ANIMAL>/$ANIMAL/g" {} \;
+
+# Replace <ENV-IP>
+find examples* -type f -exec sed -i -e "s/<ENV-IP>/$HOST_IP/g" {} \;
+
+# Replace <ENV-NAME>
+find examples* -type f -exec sed -i -e "s/<ENV-NAME>/$ENVIRONMENT/g" {} \;
+
+# Replace <ENV-DOMAIN>
+find examples* -type f -exec sed -i -e "s/<ENV-DOMAIN>/$DOMAIN/g" {} \;
